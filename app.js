7d41b33ac8f914a398051b1063216fd6696e1aeb
“nupurparmar6” <nupurparmar6@gmail.com>
2021-02-11 19:53:42 +0530

    add functionality to display images

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+
 const form= document.querySelector("#form");
 
 form.addEventListener("submit", async function(event){
     event.preventDefault(); //will prevent refreshing of the page on clicking the button
     let input= form.elements.query.value;
-    let url= `http://api.tvmaze.com/search/shows?q=${input}`;
-    let res= await axios.get(url);
-    
-    let img= document.createElement("img");
-    img.src= res.data[0].show.image.medium;
-    document.body.append(img);
+    let url= `http://api.tvmaze.com/search/shows?q=`;
+    let config= {params:{q:input}};
+    let res= await axios.get(url,config);
+    createImages(res.data);
+    form.elements.query.value= "";
+
 })
+
+function createImages(arr){
+    
+    for(let i=0; i<arr.length; i++){
+        if(arr[i].show.image){
+            // console.log(arr[i].show.image.medium);
+            let img= document.createElement("img");
+            img.src= arr[i].show.image.medium;
+            document.body.append(img);
+        } 
+    }
+}
